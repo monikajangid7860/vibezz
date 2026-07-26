61b058f5a2a1fb83d558d8ce7a13dc7ce6b1498b
monikajangid7860 <monikajangid93143@gmail.com>
2026-07-27 01:42:15 +0530

    Initial deployment

--- components/BagelPhysics/physicsConfig.js
+++ components/BagelPhysics/physicsConfig.js
@@ -2,7 +2,7 @@ export const PHYSICS_CONFIG = {
   gravity: 1,
   spawnInterval: 350,
   maxObjects: 34,
-  objectSizeRange: [180, 260],
+  objectSizeRange: [100, 160],
   restitution: [0.16, 0.28],
   friction: [0.65, 0.9],
   density: 0.0014,
